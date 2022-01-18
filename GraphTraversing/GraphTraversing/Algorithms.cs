34534a7f8d619d64f9e3f34938915b3e7c97e04c
TabishRiazBajwa <97871244+TabishRiazBajwa@users.noreply.github.com>
2022-01-18 22:52:27 +0500

    1st upload

---
+++ GraphTraversing/GraphTraversing/Algorithms.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphTraversing
+{
+    public class Algorithms
+    {
+        public HashSet<T> DFS<T>(Graph<T> graph, T start)
+        {
+            var visited = new HashSet<T>();
+
+            if (!graph.AdjacencyList.ContainsKey(start))
+                return visited;
+
+            var stack = new Stack<T>();
+            stack.Push(start);
+
+            while (stack.Count > 0)
+            {
+                var vertex = stack.Pop();
+
+                if (visited.Contains(vertex))
+                    continue;
+
+                visited.Add(vertex);
+
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                    if (!visited.Contains(neighbor))
+                        stack.Push(neighbor);
+            }
+            return visited;
+        }
+        public HashSet<T> BFS<T>(Graph<T> graph, T start)
+        {
+            var visited = new HashSet<T>();
+
+            if (!graph.AdjacencyList.ContainsKey(start))
+                return visited;
+
+            var queue = new Queue<T>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+
+                if (visited.Contains(vertex))
+                    continue;
+
+                visited.Add(vertex);
+
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                    if (!visited.Contains(neighbor))
+                        queue.Enqueue(neighbor);
+            }
+
+            return visited;
+        }
+    }
+}
---
+++ GraphTraversing/GraphTraversing/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+
+namespace GraphTraversing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vertices2 = new[] { 0, 1, 2, 3, 4, 5 };
+            var edges2 = new[]{ Tuple.Create(0,1) ,Tuple.Create(1,2), Tuple.Create(1,5),
+                Tuple.Create(2,3), Tuple.Create(2,4), Tuple.Create(4,5)};
+            var graph2 = new Graph<int>(vertices2, edges2);
+            var vertices = new[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+            var edges = new[]{ Tuple.Create('A','B') ,Tuple.Create('B','C'), Tuple.Create('B','E'),
+                Tuple.Create('C','A'), Tuple.Create('C','D'), Tuple.Create('C','G'), Tuple.Create('D','A'),
+                Tuple.Create('E','C'), Tuple.Create('E','F'), Tuple.Create('F','G')};
+            var graph = new Graph<char>(vertices, edges);
+            var algorithms = new Algorithms();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Perform Depth First Traversal");
+                Console.WriteLine("2. Perform Breadth First Traversal");
+                Console.WriteLine("3. Search Graph 1 (DFS)");
+                Console.WriteLine("4. Search Graph 2 (BFS)");
+                Console.WriteLine("5. Exit");
+                var option = Console.ReadLine();
+                if (option == "1")
+                {
+                    Console.WriteLine("1. Perform Depth First Traversal on Graph 1");
+                    Console.WriteLine("2. Perform Depth First Traversal on Graph 2");
+                    option = Console.ReadLine();
+                    if (option == "1")
+                    {
+                        Console.WriteLine(string.Join(", ", algorithms.DFS(graph, 'A')));
+                        Console.WriteLine("Press Enter to continue");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Join(", ", algorithms.DFS(graph2, 0)));
+                        Console.WriteLine("Press Enter to continue");
+                        Console.ReadLine();
+                    }
+                }
+                else if (option == "2")
+                {
+                    Console.WriteLine("1. Perform Breadth First Traversal on Graph 1");
+                    Console.WriteLine("2. Perform Breadth First Traversal on Graph 2");
+                    option = Console.ReadLine();
+                    if (option == "1")
+                    {
+                        Console.WriteLine(string.Join(", ", algorithms.BFS(graph, 'A')));
+                        Console.WriteLine("Press Enter to continue");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Join(", ", algorithms.BFS(graph2, 0)));
+                        Console.WriteLine("Press Enter to continue");
+                        Console.ReadLine();
+                    }
+                }
+                else if (option == "3")
+                {
+                    Console.WriteLine("Please Input the Node");
+                    option = Console.ReadLine();
+                    if (vertices.Any(x => x.Equals(Convert.ToChar(option))))
+                    {
+                        Console.WriteLine(string.Join(", ", algorithms.DFS(graph, Convert.ToChar(option))));
+                        Console.WriteLine("Press Enter to continue");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine(option + " is not a Node");
+                        Console.WriteLine("Press Enter to continue");
+                        Console.ReadLine();
+                    }
+                }
+                else if (option == "4")
+                {
+                    Console.WriteLine("Please Input the Node");
+                    option = Console.ReadLine();
+                    if (option=="0" || option == "1" || option == "2" || option == "3" || option == "4" 
+                        || option == "5")
+                    {
+                        Console.WriteLine(string.Join(", ", algorithms.BFS(graph2, Convert.ToInt32(option))));
+                        Console.WriteLine("Press Enter to continue");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine(option + " is not a Node");
+                        Console.WriteLine("Press Enter to continue");
+                        Console.ReadLine();
+                    }
+                }
+                else if (option == "5")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
